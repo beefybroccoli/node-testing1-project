953a6c28c561c9d184c01b03ce39c04357d517a6
beefybroccoli <tedkimemail@yahoo.com>
2021-12-15 12:01:23 -0800

    ...

--- index.js
+++ index.js
@@ -7,7 +7,12 @@
  * trimProperties({ name: '  jane  ' }) // returns a new object { name: 'jane' }
  */
 function trimProperties(obj) {
-  // ✨ implement
+  for ( let key of Object.keys(obj)){
+    if (typeof obj[key] === 'string'){
+      obj[key] = obj[key].trim();
+    }
+  }
+  return obj;
 }
 
 /**
